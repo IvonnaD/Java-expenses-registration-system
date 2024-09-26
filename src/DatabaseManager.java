b0e1b1a9c5719c4d2342154b8659058f47471c66
Ivonna D <dabolinai@gmail.com>
2024-09-26 16:59:46 +0300

    Added basic structure elements

--- src/DatabaseManager.java
+++ src/DatabaseManager.java
@@ -1,3 +1,6 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
 import java.sql.SQLException;
 
 public class DatabaseManager {
@@ -5,9 +8,26 @@ public class DatabaseManager {
     private static final String USER = "root";
     private static final String PASSWORD = "";
 
-    //connecting to the databse
+//connecting to the databse
     public Connection connect() throws SQLException{
         return DriverManager.getConnection(URL, USER, PASSWORD);
 
+    }
+//adding entry of expenses to the database
+    public void addExpense(String date, double sum, String description) {
+        String sql = "INSERT INTO expsenses (expense_date, sum, description) VALUES (?, ?, ?)";
+//values empty for now
+
+        try (Connection conn = connect();
+             PreparedStatement stmt = conn.prepareStatement(sql)) {
+            stmt.setString(1, date);
+            stmt.setString(2, sum);
+            stmt.setString(3, description);
+//to insert expense entry into the database
+            stmt.executeUpdate();
+        } catch (SQLException e){
+            e.printStackTrace();
+//to catch in case error happens
+        }
     }
 }
